4cf70d3e244681b7c723b96e09526dd8ef1c5eec
kalle hålldén <kallehalldeen@gmail.com>
2019-09-23 15:08:17 +0200

    Flutter project created

---
+++ App/todoapp/android/app/src/main/kotlin/com/example/todoapp/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.todoapp
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
